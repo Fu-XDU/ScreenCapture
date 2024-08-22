71d36b6266d4243326da9894a29e0598c333f0b0
Fu-XDU <fuming.137@icloud.com>
2024-08-22 13:03:24 +0800

    Initial Commit

---
+++ ScreenCapture/ScreenCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenCaptureApp.swift
+//  ScreenCapture
+//
+//  Created by 付铭 on 2024/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ScreenCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
